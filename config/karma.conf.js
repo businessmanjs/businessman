7d36b3ccfc1efb4d50f735552f2e389e1e0d8ed8
aggre <hiroyuki.aggre@gmail.com>
2016-12-26 18:22:20 +0900

    Add test modules

---
+++ config/karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function ( config ) {
+    config.set( {
+        basePath: '../',
+        frameworks: [ 'mocha' ],
+        plugins: [
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher'
+        ],
+        files: [
+            'node_modules/expect.js/index.js',
+            'dist/businessman.js',
+            'test/businessman.js'
+        ],
+        browsers: [ 'Chrome', 'Firefox' ],
+        reporters: [ 'mocha' ],
+        singleRun: true
+    } )
+}
