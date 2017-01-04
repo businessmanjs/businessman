d1e2facc9ce08c3abd54353b4a551c986697ecb0
aggre <hiroyuki.aggre@gmail.com>
2017-01-05 08:04:48 +0900

    Try ava

--- test/businessman.js
+++ test/businessman.js
@@ -1,119 +1,107 @@
+import test from 'ava'
 import {install, dispatch, operate, subscribe, unsubscribe, getState} from '../src/businessman'
 
-describe('businessman specs', function () {
-	var stores
+var stores
 
-	it('Install Worker', function (done) {
-		subscribe('CREATE_CLIENT_STORE', data => {
-			stores = data
-			expect(data).to.be.ok()
-			done()
-		})
-		install('/dist/test-worker.js')
+test('Install Worker', t => {
+	subscribe('CREATE_CLIENT_STORE', data => {
+		stores = data
+		t.truthy(data)
 	})
+	install('/dist/test-worker.js')
+})
 
-	it('Store for clients includes dispatch() and subscribe()', function () {
-		expect(stores).to.be.ok()
-		let storeKeys = Object.keys(stores)
-		for (let i = 0; i < storeKeys.length; i++) {
-			let store = storeKeys[i]
-			expect(stores[store]).to.have.property('dispatch')
-			expect(stores[store]).to.have.property('subscribe')
-			expect(stores[store].dispatch).to.be.an('function')
-			expect(stores[store].subscribe).to.be.an('function')
-		}
+test('Store for clients includes dispatch() and subscribe()', t => {
+	let storeKeys = Object.keys(stores)
+	for (let i = 0; i < storeKeys.length; i++) {
+		let store = storeKeys[i]
+		t.true('dispatch' in stores[store])
+		t.true('subscribe' in stores[store])
+	}
+})
+
+test('Dispatch and subscribe from the client store', t => {
+	stores.counter.dispatch('increment', 1)
+	stores.counter.subscribe((state, applied) => {
+		t.is(state, 1)
+		t.is(applied, 'increment')
 	})
-
-	it('Dispatch and subscribe from the client store', function (done) {
-		stores.counter.dispatch('increment', 1)
-		stores.counter.subscribe(function (state, applied) {
-			expect(state).to.be(1)
-			expect(applied).to.be('increment')
-		})
-		stores.message.dispatch('set', 'This is a test')
-		stores.message.subscribe(function (state, applied) {
-			expect(state).to.be('This is a test')
-			expect(applied).to.be('set')
-			done()
-		})
+	stores.message.dispatch('set', 'This is a test')
+	stores.message.subscribe((state, applied) => {
+		t.is(state, 'This is a test')
+		t.is(applied, 'set')
 	})
+})
 
-	it('Unsubscribe from the client store', function (done) {
-		stores.counter.unsubscribe()
-		let i = 0
-		let counterSubscriber = function () {
-			i++
-		}
-		stores.counter.subscribe(counterSubscriber)
+test('Unsubscribe from the client store', t => {
+	stores.counter.unsubscribe()
+	let i = 0
+	let counterSubscriber = function () {
+		i++
+	}
+	stores.counter.subscribe(counterSubscriber)
+	stores.counter.dispatch('increment')
+	setTimeout(() => {
+		stores.counter.unsubscribe(counterSubscriber)
 		stores.counter.dispatch('increment')
-		setTimeout(function () {
-			stores.counter.unsubscribe(counterSubscriber)
-			stores.counter.dispatch('increment')
-			expect(i).to.be(1)
-			done()
-		}, 500)
-	})
+		t.is(i, 1)
+	}, 500)
+})
 
-	it('Get store state', function (done) {
-		stores.counter.unsubscribe()
-		stores.counter.dispatch('set', 123456)
-		stores.counter.getState().then(state => {
-			expect(state).to.be(123456)
-			done()
+test('Get store state', t => {
+	stores.counter.unsubscribe()
+	stores.counter.dispatch('set', 123456)
+	stores.counter.getState().then(state => {
+		t.is(state, 123456)
+	})
+})
+
+test('Dispatch and subscribe from the Businessman', t => {
+	dispatch('counter', 'set', 0)
+	setTimeout(() => {
+		dispatch('counter', 'increment', 1)
+		subscribe('counter', (state, applied) => {
+			t.is(state, 1)
+			t.is(applied, 'increment')
 		})
-	})
+		dispatch('message', 'set', 'This is a test')
+		subscribe('message', (state, applied) => {
+			t.is(state, 'This is a test')
+			t.is(applied, 'set')
+		})
+	}, 500)
+})
 
-	it('Dispatch and subscribe from the Businessman', function (done) {
-		dispatch('counter', 'set', 0)
-		setTimeout(() => {
-			dispatch('counter', 'increment', 1)
-			subscribe('counter', function (state, applied) {
-				expect(state).to.be(1)
-				expect(applied).to.be('increment')
-			})
-			dispatch('message', 'set', 'This is a test')
-			subscribe('message', function (state, applied) {
-				expect(state).to.be('This is a test')
-				expect(applied).to.be('set')
-				done()
-			})
-		}, 500)
-	})
-
-	it('Unsubscribe from the Businessman', function (done) {
-		unsubscribe('counter')
-		let i = 0
-		let counterSubscriber = function () {
-			i++
-		}
-		subscribe('counter', counterSubscriber)
+test('Unsubscribe from the Businessman', t => {
+	unsubscribe('counter')
+	let i = 0
+	let counterSubscriber = () => {
+		i++
+	}
+	subscribe('counter', counterSubscriber)
+	dispatch('counter', 'increment')
+	setTimeout(() => {
+		unsubscribe('counter', counterSubscriber)
 		dispatch('counter', 'increment')
-		setTimeout(function () {
-			unsubscribe('counter', counterSubscriber)
-			dispatch('counter', 'increment')
-			expect(i).to.be(1)
-			done()
-		}, 500)
-	})
+		t.is(i, 1)
+	}, 500)
+})
 
-	it('Get store state from the Businessman', function (done) {
-		unsubscribe('counter')
-		dispatch('counter', 'set', 123456789)
-		getState('counter').then(state => {
-			expect(state).to.be(123456789)
-			done()
-		})
+test('Get store state from the Businessman', t => {
+	unsubscribe('counter')
+	dispatch('counter', 'set', 123456789)
+	getState('counter').then(state => {
+		t.is(state, 123456789)
 	})
+})
 
-	it('Execute action crossed to multiple stores by manager', function (done) {
-		operate('countUpMessage', 1)
-		stores.counter.subscribe(state => {
-			expect(state).to.be(123456789 + 1)
-		})
-		stores.message.unsubscribe()
-		stores.message.subscribe(state => {
-			expect(state).to.be('1 has been added to the counter')
-			done()
-		})
+test('Execute action crossed to multiple stores by manager', t => {
+	operate('countUpMessage', 1)
+	stores.counter.subscribe(state => {
+		t.is(state, 123456789 + 1)
+	})
+	stores.message.unsubscribe()
+	stores.message.subscribe(state => {
+		t.is(state, '1 has been added to the counter')
 	})
 })
